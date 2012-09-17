d5348418b4619d214baa11148e325dd2bbd34ec3
Philip Ashlock <phil@civicagency.org>
2012-09-17 13:10:12 -0400

    Updating Documentation

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -50,9 +50,9 @@ code {
 <p>Currently this demo is limited to the United States</p>
 
 
-<h1>GeoWebDNS Endpoints</h1>
+<h1>GeoWeb DNS Endpoints</h1>
 
-URL: http://api.democracymap.org/geowebdns/endpoints
+<pre>URL: http://api.democracymap.org/geowebdns/endpoints</pre>
 
 <p>There are just a few parameters</p>
 
@@ -62,11 +62,29 @@ URL: http://api.democracymap.org/geowebdns/endpoints
 	<li><strong>geojson</strong> <em>(options: true, false).</em> This specifies whether you want a geojson representation of the boundary returned</li>
 </ul>
 
-<h2>Example Call</h2>
+<h3>Example Call</h3>
 <p>
 	<a href="http://api.democracymap.org/geowebdns/endpoints?location=chicago&format=json&geojson=true">http://api.democracymap.org/geowebdns/endpoints?location=chicago&amp;format=json&amp;geojson=true</a>
 </p>	
 
+<h1>Source and More information</h1>
+<p>You can find a description of <a href="http://wiki.open311.org/GeoWeb_DNS">GeoWeb DNS on the Open311 wiki</a>. The source code for this is <a href="https://github.com/philipashlock/codeigniter-restserver">available on github</a>. This is experimental, don't expect it to stick around</p>
+
+<h1>Credits</h1>
+<p>
+This is built on the same data as the main DemocracyMap site, so I repeating that here. City data from U.S. Census including the <a href="http://www.census.gov/govs/cog/">2007 Census of Governments</a>. 
+US Congressional data from the <a href="http://services.sunlightlabs.com/docs/Sunlight_Congress_API/">Sunlight Congress API</a>. 
+Congressional district boundaries from <a href="http://www.govtrack.us/congress/findyourreps.xpd">GovTrack.us</a>. 
+State legislative data from the <a href="http://openstates.org/api/">OpenStates API</a>. 
+NYC data <em>scraped</em> from <a href="http://nyc.gov">nyc.gov</a>. NYC boundary data from <a href="http://www.nyc.gov/data">NYC OpenData</a>. 
+City-level geospatial data served by <a href="http://geoserver.org">GeoServer</a>. 
+All geocoding courtesy of <a href="http://developer.yahoo.com/geo/placefinder/">Yahoo! PlaceFinder</a>. 
+Tweet stream uses the <a href="http://tweet.seaofclouds.com/">Tweet! jQuery plugin from seaofclouds</a>. 
+</p>
+
+<p> 
+	Please check out the  <a href="http://lists.open311.org/groups/discuss/">Open311 mailing list</a> and the <a href="http://forums.e-democracy.org/groups/democracymap">DemocracyMap mailing list</a>.
+</p>
 
 </body>
 </html>
