6ec5731f3077aa1a0d4b632d1714560c996c1dd1
Philip Ashlock <phil@civicagency.org>
2012-09-17 13:17:48 -0400

    updating more links in docs

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,11 +68,11 @@ code {
 </p>	
 
 <h1>Source and More information</h1>
-<p>You can find a description of <a href="http://wiki.open311.org/GeoWeb_DNS">GeoWeb DNS on the Open311 wiki</a>. The source code for this is <a href="https://github.com/philipashlock/democracymap-restserver">available on github</a>. This is experimental, don't expect it to stick around</p>
+<p>You can find a description of <a href="http://wiki.open311.org/GeoWeb_DNS">GeoWeb DNS on the Open311 wiki</a>. The source code for this is <a href="https://github.com/philipashlock/democracymap-restserver">available on github</a>. This is experimental, don't expect it to be accurate or to stick around</p>
 
 <h1>Credits</h1>
 <p>
-This is built on the same data as the main DemocracyMap site, so I repeating that here. City data from U.S. Census including the <a href="http://www.census.gov/govs/cog/">2007 Census of Governments</a>. 
+This is built on the same data as the main <a href="http://beta.democracymap.org">DemocracyMap demo</a>, so I'm repeating that here. City data from U.S. Census including the <a href="http://www.census.gov/govs/cog/">2007 Census of Governments</a>. 
 US Congressional data from the <a href="http://services.sunlightlabs.com/docs/Sunlight_Congress_API/">Sunlight Congress API</a>. 
 Congressional district boundaries from <a href="http://www.govtrack.us/congress/findyourreps.xpd">GovTrack.us</a>. 
 State legislative data from the <a href="http://openstates.org/api/">OpenStates API</a>. 
