16814e5c18f27598188de66a7464dfb25fb1e625
Philip Ashlock <phil@civicagency.org>
2013-03-05 16:22:01 -0500

    Getting full state name and thus ensuring we get governor too

--- application/models/geocoder_model.php
+++ application/models/geocoder_model.php
@@ -41,6 +41,9 @@ class Geocoder_model extends CI_Model {
 			$data['city_geocoded'] 		= $location['results'][0]['locations'][0]['adminArea5'];
 			$data['state_geocoded'] 	= $location['results'][0]['locations'][0]['adminArea3'];
 
+			// Convert state abbreviation to full name
+			$data['state_geocoded']		= $this->state_abbr($data['state_geocoded']); 
+
 			return $data;
 
 		}	else {
@@ -88,7 +91,242 @@ class Geocoder_model extends CI_Model {
 	}
 	
 
-	
+	function state_abbr($abbr) {
+		
+$states = '		[
+		    {
+		        "abbreviation": "AL",
+		        "state": "Alabama"
+		    },
+		    {
+		        "abbreviation": "AK",
+		        "state": "Alaska"
+		    },
+		    {
+		        "abbreviation": "0",
+		        "state": "American Samoa"
+		    },
+		    {
+		        "abbreviation": "AZ",
+		        "state": "Arizona"
+		    },
+		    {
+		        "abbreviation": "AR",
+		        "state": "Arkansas"
+		    },
+		    {
+		        "abbreviation": "CA",
+		        "state": "California"
+		    },
+		    {
+		        "abbreviation": "CO",
+		        "state": "Colorado"
+		    },
+		    {
+		        "abbreviation": "CT",
+		        "state": "Connecticut"
+		    },
+		    {
+		        "abbreviation": "DE",
+		        "state": "Delaware"
+		    },
+		    {
+		        "abbreviation": "FL",
+		        "state": "Florida"
+		    },
+		    {
+		        "abbreviation": "GA",
+		        "state": "Georgia"
+		    },
+		    {
+		        "abbreviation": "GU",
+		        "state": "Guam"
+		    },
+		    {
+		        "abbreviation": "HI",
+		        "state": "Hawaii"
+		    },
+		    {
+		        "abbreviation": "ID",
+		        "state": "Idaho"
+		    },
+		    {
+		        "abbreviation": "IL",
+		        "state": "Illinois"
+		    },
+		    {
+		        "abbreviation": "IN",
+		        "state": "Indiana"
+		    },
+		    {
+		        "abbreviation": "IA",
+		        "state": "Iowa"
+		    },
+		    {
+		        "abbreviation": "KS",
+		        "state": "Kansas"
+		    },
+		    {
+		        "abbreviation": "KY",
+		        "state": "Kentucky"
+		    },
+		    {
+		        "abbreviation": "LA",
+		        "state": "Louisiana"
+		    },
+		    {
+		        "abbreviation": "ME",
+		        "state": "Maine"
+		    },
+		    {
+		        "abbreviation": "MD",
+		        "state": "Maryland"
+		    },
+		    {
+		        "abbreviation": "MA",
+		        "state": "Massachusetts"
+		    },
+		    {
+		        "abbreviation": "MI",
+		        "state": "Michigan"
+		    },
+		    {
+		        "abbreviation": "MN",
+		        "state": "Minnesota"
+		    },
+		    {
+		        "abbreviation": "MS",
+		        "state": "Mississippi"
+		    },
+		    {
+		        "abbreviation": "MO",
+		        "state": "Missouri"
+		    },
+		    {
+		        "abbreviation": "0",
+		        "state": "Montana"
+		    },
+		    {
+		        "abbreviation": "NE",
+		        "state": "Nebraska"
+		    },
+		    {
+		        "abbreviation": "NV",
+		        "state": "Nevada"
+		    },
+		    {
+		        "abbreviation": "0",
+		        "state": "New Hampshire"
+		    },
+		    {
+		        "abbreviation": "NJ",
+		        "state": "New Jersey"
+		    },
+		    {
+		        "abbreviation": "NM",
+		        "state": "New Mexico"
+		    },
+		    {
+		        "abbreviation": "NY",
+		        "state": "New York"
+		    },
+		    {
+		        "abbreviation": "0",
+		        "state": "North Carolina"
+		    },
+		    {
+		        "abbreviation": "ND",
+		        "state": "North Dakota"
+		    },
+		    {
+		        "abbreviation": "MP",
+		        "state": "Northern Mariana Islands"
+		    },
+		    {
+		        "abbreviation": "OH",
+		        "state": "Ohio"
+		    },
+		    {
+		        "abbreviation": "OK",
+		        "state": "Oklahoma"
+		    },
+		    {
+		        "abbreviation": "OR",
+		        "state": "Oregon"
+		    },
+		    {
+		        "abbreviation": "PA",
+		        "state": "Pennsylvania"
+		    },
+		    {
+		        "abbreviation": "0",
+		        "state": "Puerto Rico"
+		    },
+		    {
+		        "abbreviation": "RI",
+		        "state": "Rhode Island"
+		    },
+		    {
+		        "abbreviation": "SC",
+		        "state": "South Carolina"
+		    },
+		    {
+		        "abbreviation": "SD",
+		        "state": "South Dakota"
+		    },
+		    {
+		        "abbreviation": "TN",
+		        "state": "Tennessee"
+		    },
+		    {
+		        "abbreviation": "TX",
+		        "state": "Texas"
+		    },
+		    {
+		        "abbreviation": "UT",
+		        "state": "Utah"
+		    },
+		    {
+		        "abbreviation": "VT",
+		        "state": "Vermont"
+		    },
+		    {
+		        "abbreviation": "VI",
+		        "state": "Virgin Islands"
+		    },
+		    {
+		        "abbreviation": "VA",
+		        "state": "Virginia"
+		    },
+		    {
+		        "abbreviation": "WA",
+		        "state": "Washington"
+		    },
+		    {
+		        "abbreviation": "WV",
+		        "state": "West Virginia"
+		    },
+		    {
+		        "abbreviation": "WI",
+		        "state": "Wisconsin"
+		    },
+		    {
+		        "abbreviation": "WY",
+		        "state": "Wyoming"
+		    }
+		]';		
+		
+		
+		$states = json_decode($states);
+			
+		foreach ($states as $state) {
+			if ($abbr == $state->abbreviation) {
+				return $state->state;
+			}
+				
+		}
+		
+	}
 	
 	
 
