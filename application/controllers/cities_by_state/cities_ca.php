07c5a83d69b569a9d212f8aebd34bfafad496ce7
Philip Ashlock <phil@ashlock.us>
2015-03-11 22:17:05 -0400

    verify California city data is available

--- application/controllers/cities_by_state/cities_ca.php
+++ application/controllers/cities_by_state/cities_ca.php
@@ -16,42 +16,45 @@ $url ="https://api.scraperwiki.com/api/1.0/datastore/sqlite?format=jsondict&name
 
 $officials = curl_to_json($url);
 
-// normalized data
-foreach ($officials as $official) {
-		 			
-	if (strripos($official['title'], 'mayor') !== false) { // Had considered these tests too, but don't think they're elected: OR strripos($official['title'], 'city manager') !== false OR strripos($official['title'], 'executive') !== false
-		$elected_type = 'executive';
-	} 
-	else if (strripos($official['title'], 'council') !== false OR strripos($official['title'], 'member') !== false OR strripos($official['title'], 'district') !== false) {
-		$elected_type = 'legislative';
-	} else {
-		$elected_type = 'administrative';
-	}
-		
-	$electeds[] = $this->elected_official_model($type = $elected_type, 
-											  $title = $official['title'], 
-											  $description = null, 
-											  $name_given = null, 
-											  $name_family = null, 
-											  $name_full = $official['name_full'], 
-											  $url = null, 
-											  $url_photo = null, 
-											  $url_schedule = null, 
-											  $url_contact = null, 
-											  $email = null, 
-											  $phone = $official['phone'], 
-											  $address_name = null, 
-											  $address_1 = $official['address_1'], 
-											  $address_2 = $official['address_2'], 
-											  $address_city = $official['city'], 
-											  $address_state = $official['state'], 
-											  $address_zip = $official['zip'], 
-											  $current_term_enddate = null, 
-											  $last_updated = null, 
-											  $social_media = null);
+if(!empty($officials)) {
 	
-}
+	// normalized data
+	foreach ($officials as $official) {
+			 			
+		if (strripos($official['title'], 'mayor') !== false) { // Had considered these tests too, but don't think they're elected: OR strripos($official['title'], 'city manager') !== false OR strripos($official['title'], 'executive') !== false
+			$elected_type = 'executive';
+		} 
+		else if (strripos($official['title'], 'council') !== false OR strripos($official['title'], 'member') !== false OR strripos($official['title'], 'district') !== false) {
+			$elected_type = 'legislative';
+		} else {
+			$elected_type = 'administrative';
+		}
+			
+		$electeds[] = $this->elected_official_model($type = $elected_type, 
+												  $title = $official['title'], 
+												  $description = null, 
+												  $name_given = null, 
+												  $name_family = null, 
+												  $name_full = $official['name_full'], 
+												  $url = null, 
+												  $url_photo = null, 
+												  $url_schedule = null, 
+												  $url_contact = null, 
+												  $email = null, 
+												  $phone = $official['phone'], 
+												  $address_name = null, 
+												  $address_1 = $official['address_1'], 
+												  $address_2 = $official['address_2'], 
+												  $address_city = $official['city'], 
+												  $address_state = $official['state'], 
+												  $address_zip = $official['zip'], 
+												  $current_term_enddate = null, 
+												  $last_updated = null, 
+												  $social_media = null);
+		
+	}
 
+}
 
 
 ?>
